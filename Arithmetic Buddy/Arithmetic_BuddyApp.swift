8cce25148de4f68fc4497ad038cdf0ac0dcb41ea
Joseph Ji <jji@lcs.on.ca>
2025-01-18 10:12:02 -0500

    Initial Commit

---
+++ Arithmetic Buddy/Arithmetic_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_BuddyApp.swift
+//  Arithmetic Buddy
+//
+//  Created by Xinchen Ji on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
